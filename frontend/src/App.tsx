8daaf22ff92635d3081e2755be6772e2241c030a
gaurondorian@gmail.com <gaurondorian@gmail.com>
2025-06-03 14:56:20 -0400

    :recycle: Moove UI to frontend folder

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import DragAndDrop from "./components/drag-and-drop/drag-and-drop";
+
+function App() {
+  return (
+    <>
+      <DragAndDrop />
+    </>
+  );
+}
+
+export default App;
