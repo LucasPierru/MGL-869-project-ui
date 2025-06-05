6a08ea9ae695166dfb1f932e001f1384c66147b2
gaurondorian@gmail.com <gaurondorian@gmail.com>
2025-06-05 15:34:01 -0400

    :recycle: Refacto CNN

--- ML/learnings/cnn.py
+++ ML/learnings/cnn.py
@@ -1,10 +1,10 @@
-import tensorflow as tf
 from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization,Conv2D, MaxPooling2D, Input, GlobalAveragePooling2D
+from tensorflow.keras.layers import MaxPool2D,Conv2D, Dense, Dropout, Activation, Flatten, BatchNormalization,Conv2D, MaxPooling2D, Input, GlobalAveragePooling2D
 from tensorflow.keras.regularizers import l2
 import numpy as np
+import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
 import cv2
@@ -13,30 +13,73 @@ from tensorflow.keras.applications.efficientnet import preprocess_input
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import cifar10 as Cifar10
+from keras.callbacks import LearningRateScheduler, EarlyStopping
+
 
 class CNN(Cifar10.Cifar10):
+    KERNEL_SIZE = (3, 3)
 
     def compile_model(self):
-        self.model = tf.keras.Sequential([
+        model = Sequential()
 
-            tf.keras.layers.Conv2D(30, 3, activation="relu", input_shape=self.input_shape),
-            tf.keras.layers.MaxPool2D(pool_size=2),
-            tf.keras.layers.Conv2D(30, 3, activation="relu"),
+        # Convolutional Layer
+        model.add(Conv2D(filters=32, kernel_size=CNN.KERNEL_SIZE, input_shape=self.input_shape ,activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(filters=32, kernel_size=CNN.KERNEL_SIZE, input_shape=self.input_shape ,activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        # Pooling layer
+        model.add(MaxPool2D(pool_size=(2, 2)))
+        # Dropout layers
+        model.add(Dropout(0.25))
 
-            tf.keras.layers.MaxPool2D(pool_size=2),
-            tf.keras.layers.Conv2D(30, 3, activation="relu"),
+        model.add(Conv2D(filters=64, kernel_size=CNN.KERNEL_SIZE, input_shape=self.input_shape, activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(filters=64, kernel_size=CNN.KERNEL_SIZE, input_shape=self.input_shape, activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPool2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
-            tf.keras.layers.MaxPool2D(),
-            tf.keras.layers.Flatten(),
+        model.add(Conv2D(filters=128, kernel_size=CNN.KERNEL_SIZE, input_shape=self.input_shape ,activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(Conv2D(filters=128, kernel_size=CNN.KERNEL_SIZE, input_shape=self.input_shape ,activation='relu', padding='same'))
+        model.add(BatchNormalization())
+        model.add(MaxPool2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
 
-            tf.keras.layers.Dense(self.num_classes, activation="softmax")
-        ])
+        model.add(Flatten())
+        # model.add(Dropout(0.2))
+        model.add(Dense(128, activation='relu'))
+        model.add(Dropout(0.25))
+        model.add(Dense(10, activation='softmax'))
 
-        self.model.compile(loss="sparse_categorical_crossentropy",optimizer="adam",metrics=["accuracy"])
+        model.compile(
+            optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy']
+        )
+
+    def lr_scheduler(epoch, lr):
+        if epoch < 10:
+            return lr
+        else:
+            return float(lr * tf.math.exp(-0.1))
 
     def train_model(self):
+
+        callbacks = [
+            EarlyStopping(patience=5, monitor="val_loss", restore_best_weights=True),
+            LearningRateScheduler(self.lr_scheduler)
+        ]
+
         (train_data, valid_data) = self.data_augmentation()
-        self.history = self.model.fit(train_data,epochs=20,batch_size=32,validation_data=valid_data, callbacks=[self.early_stopping_callback])
+
+        self.history = self.model.fit(
+            train_data,
+            epochs=50,
+            batch_size=64,
+            validation_data=valid_data,
+            callbacks=callbacks
+        )
 
 if __name__ == "__main__":
     cnn = CNN()
