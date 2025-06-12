1112ed715ab44cf0b7d432e0f982a382e1474dd5
Lucas Pierru <lucaspierru7@gmail.com>
2025-06-12 11:21:46 -0400

    Added title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import DragAndDrop from "./components/drag-and-drop/drag-and-drop";
 function App() {
   return (
     <>
+      <h1 className="text-3xl font-semibold">Image Classification - Cifar 10</h1>
       <DragAndDrop />
     </>
   );
